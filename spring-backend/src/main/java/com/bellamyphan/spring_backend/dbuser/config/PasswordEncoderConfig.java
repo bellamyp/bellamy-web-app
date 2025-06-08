cfee46b664acd6a47cff3dea5467d91f092ba50e
bellamyphan <bellamyphan@icloud.com>
2025-06-08 12:17:21 -0500

    backend: Added Spring Boot security, and define BCryptPasswordEncoder bean

---
+++ spring-backend/src/main/java/com/bellamyphan/spring_backend/dbuser/config/PasswordEncoderConfig.java
@@ -0,0 +1,19 @@
+package com.bellamyphan.spring_backend.dbuser.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(PasswordEncoderConfig.class);
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        logger.info("Creating BCryptPasswordEncoder bean manually because other beans will share the same instance");
+        return new BCryptPasswordEncoder();
+    }
+}
