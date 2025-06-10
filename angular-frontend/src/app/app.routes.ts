6d731beac029e80ee0ec3402f771f144720ddec1
BellamyPhan <bellamyphan@icloud.com>
2025-06-10 18:31:35 -0500

    frontend: create main menu component

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainMenuComponent } from './feature/menu/components/main-menu/main-menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/main-menu', pathMatch: 'full' },
+    { path: 'main-menu', component: MainMenuComponent }
+];
