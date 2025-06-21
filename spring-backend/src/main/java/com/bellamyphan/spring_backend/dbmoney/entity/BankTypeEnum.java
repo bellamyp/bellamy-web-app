76c77073e6b77413c73c904d91e4d741aaeed05f
bellamyphan <bellamyphan@icloud.com>
2025-06-21 08:06:40 -0500

    backend: create BankTypeEnum

---
+++ spring-backend/src/main/java/com/bellamyphan/spring_backend/dbmoney/entity/BankTypeEnum.java
@@ -0,0 +1,17 @@
+package com.bellamyphan.spring_backend.dbmoney.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum BankTypeEnum {
+    CHECKING("Checking"),
+    SAVINGS("Savings"),
+    CREDIT("Credit"),
+    REWARDS("Rewards");
+
+    private final String type;
+
+    BankTypeEnum(String type) {
+        this.type = type;
+    }
+}
