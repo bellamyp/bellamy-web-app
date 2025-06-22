b0823d386c44851f29bf7ff3154b943f487b733f
BellamyPhan <bellamyphan@icloud.com>
2025-06-22 14:03:18 -0500

    frontend: bank-create component, fetch bankTypes from the backend

--- angular-frontend/src/app/feature/bank/components/bank-create/bank-create.component.ts
+++ angular-frontend/src/app/feature/bank/components/bank-create/bank-create.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BankCreate } from '../../model/bank-create';
+import { BankService } from '../../service/bank.service';
 
 @Component({
   selector: 'app-bank-create',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './bank-create.component.html',
   styleUrl: './bank-create.component.scss'
 })
-export class BankCreateComponent {
+export class BankCreateComponent implements OnInit {
+
+  bank: BankCreate = {
+    name: '',
+    type: '',
+    openingDate: new Date(),
+  }
+
+  // bankTypes signal, initially empty array
+  bankTypes = signal<string[]>([]);
+
+  constructor(
+    private bankService: BankService
+  ) { }
+
+  ngOnInit(): void {
+    this.loadBankTypes();
+  }
+
+  onSubmit(): void {
+    // Handle form submission logic here
+  }
+
+  private loadBankTypes(): void {
+    this.bankService.getBankTypes().subscribe({
+      next: (types) => {
+        this.bankTypes.set(types)
+        
+        // Debugging output to verify bank types
+        console.log('BankTypes:', this.bankTypes());
+      },
+      error: (err) => console.error('Failed to load bank types', err)
+    });
+  }
 
 }
--- angular-frontend/src/app/feature/bank/service/bank.service.ts
+++ angular-frontend/src/app/feature/bank/service/bank.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BackendConfig } from '../../../core/config/backend-config';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BankService {
 
-  constructor() { }
+  private baseUrl = BackendConfig.springApiUrl; 
+  private bankUrl = `${this.baseUrl}/api/banks`; 
+  private bankTypeUrl = `${this.bankUrl}/types`;
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getBankTypes(): Observable<string[]> {
+    return this.httpClient.get<string[]>(`${this.bankTypeUrl}`);
+  }
 }
