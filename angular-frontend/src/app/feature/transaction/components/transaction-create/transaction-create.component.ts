d473822ff37e62cd8fbe475e69a818cca7c2f9bb
bellamyphan <bellamyphan@icloud.com>
2025-06-24 18:38:04 -0500

    frontend: create transaction-create component

---
+++ angular-frontend/src/app/feature/transaction/components/transaction-create/transaction-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-create',
+  imports: [],
+  templateUrl: './transaction-create.component.html',
+  styleUrl: './transaction-create.component.scss'
+})
+export class TransactionCreateComponent {
+
+}
