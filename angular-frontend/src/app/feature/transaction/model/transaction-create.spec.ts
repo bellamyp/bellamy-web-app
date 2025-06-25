9fc5c3e323a2beaf57d8430f90b81946bb7263a8
bellamyphan <bellamyphan@icloud.com>
2025-06-24 19:36:10 -0500

    frontend: create transaction-create dto

---
+++ angular-frontend/src/app/feature/transaction/model/transaction-create.spec.ts
@@ -0,0 +1,7 @@
+import { TransactionCreate } from './transaction-create';
+
+describe('TransactionCreate', () => {
+  it('should create an instance', () => {
+    expect(new TransactionCreate()).toBeTruthy();
+  });
+});
---
+++ angular-frontend/src/app/feature/transaction/model/transaction-create.ts
@@ -0,0 +1,10 @@
+import { BankInput } from "../../bank/model/bank-input";
+
+export class TransactionCreate {
+
+    date!: Date;
+    amount!: number;
+    type!: string;
+    notes?: string;
+    bank?: BankInput;
+}
