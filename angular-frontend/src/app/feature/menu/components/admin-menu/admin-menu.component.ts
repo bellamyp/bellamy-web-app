f640c9b410a4b70c32002295ad3ad3530eead157
bellamyphan <bellamyphan@icloud.com>
2025-06-24 21:47:14 -0500

    frontend: admin menu component, adding alert for buttons under construction

--- angular-frontend/src/app/feature/menu/components/admin-menu/admin-menu.component.ts
+++ angular-frontend/src/app/feature/menu/components/admin-menu/admin-menu.component.ts
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AdminMenuComponent {
 
+  manageUsers(): void {
+    alert('Manage Users functionality is not implemented yet.');
+  }
+
+  importData(): void {
+    alert('Import Data functionality is not implemented yet.');
+  }
+
+  exportData(): void {
+    alert('Export Data functionality is not implemented yet.');
+  }
+
+  servicesAndRoles(): void {
+    alert('Services and Roles functionality is not implemented yet.');
+  }
+
+  manageBackground(): void {
+    alert('Manage Background functionality is not implemented yet.');
+  }
+
+  monitorUserActivities(): void {
+    alert('Monitor User Activities functionality is not implemented yet.');
+  }
+
+  supportDashboard(): void {
+    alert('Support Dashboard functionality is not implemented yet.');
+  }
 }
